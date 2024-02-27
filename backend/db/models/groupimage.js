19234a37e13587391732d1ab5b0dbfca58237542
TStutsman <teagandstutsman@gmail.com>
2024-02-26 19:18:28 -0500

    add groupimage model

--- backend/db/models/groupimage.js
+++ backend/db/models/groupimage.js
@@ -4,19 +4,28 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class GroupImage extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+      GroupImage.belongsTo(models.Group);
     }
   }
   GroupImage.init({
-    groupId: DataTypes.INTEGER,
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: 'Groups'
+        }
+      },
+      onDelete: 'CASCADE'
+    },
     url: DataTypes.STRING,
-    preview: DataTypes.BOOLEAN
+    preview: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
   }, {
     sequelize,
     modelName: 'GroupImage',
