131549933254be24d54ce216ca9de7f5efb6d3bb
TStutsman <teagandstutsman@gmail.com>
2024-02-26 19:46:36 -0500

    fixed small bug in Event model

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -4,11 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       Event.belongsTo(models.Group);
 
@@ -24,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'eventId',
         onDelete: 'CASCADE',
         hooks: true
-      })
+      });
     }
   }
   Event.init({
@@ -73,7 +69,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
-    modelName: 'Events',
+    modelName: 'Event',
   });
   return Event;
 };
